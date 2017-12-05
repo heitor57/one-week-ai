f16d82fd5c958f57cd023377e2d753938e49bd2e
Heitor <heitorwerneck@hotmail.com>
2017-12-05 15:05:13 -0200

    Busca update

--- Assets/AI/Actions/SearchPoint.cs
+++ Assets/AI/Actions/SearchPoint.cs
@@ -9,30 +9,38 @@ using RAIN.Navigation.Waypoints;
 [RAINAction]
 public class SearchPoint : RAINAction
 {
-	
-	public SearchPoint(){
-		actionName = "SearchPoint";
-	}
+
+    public SearchPoint()
+    {
+        actionName = "SearchPoint";
+    }
     public override void Start(RAIN.Core.AI ai)
     {
-		base.Start(ai);
-		if (ai.WorkingMemory.GetItem<GameObject> ("search_waypoint") != null) {
-			GameObject.Destroy(ai.WorkingMemory.GetItem<GameObject> ("search_waypoint").gameObject);
-			ai.WorkingMemory.RemoveItem("search_waypoint");
-		}
-		Vector3 UltPos = ai.WorkingMemory.GetItem<GameObject> ("EnemyGoPersistent").transform.position;
-		GameObject obj = new GameObject();
-		WaypointRig WaypointRigTemp = obj.AddComponent<WaypointRig>();
-		//se Player fora do alcance
-		WaypointRigTemp.WaypointSet.SetType = 0;
-		WaypointRigTemp.WaypointSet.AddWaypoint(new Waypoint(UltPos, "UltimaPosicao"));
-		WaypointRigTemp.WaypointSet.AddWaypoint(new Waypoint(new Vector3(UltPos.x + 10f, UltPos.y, UltPos.z + 10f), "quadrado 1"));
-		WaypointRigTemp.WaypointSet.AddWaypoint(new Waypoint(new Vector3(UltPos.x + 10f, UltPos.y, UltPos.z - 10f), "quadrado 2"));
-		WaypointRigTemp.WaypointSet.AddWaypoint(new Waypoint(new Vector3(UltPos.x - 10f, UltPos.y, UltPos.z - 10f), "quadrado 3"));
-		WaypointRigTemp.WaypointSet.AddWaypoint(new Waypoint(new Vector3(UltPos.x - 10f, UltPos.y, UltPos.z + 10f), "quadrado 4"));
-		WaypointRigTemp.WaypointSet.AddWaypoint(new Waypoint(new Vector3(UltPos.x + 10f, UltPos.y, UltPos.z + 10f), "quadrado 5"));
+        base.Start(ai);
+        if (ai.WorkingMemory.GetItem<GameObject>("search_waypoint") != null)
+        {
+            GameObject.Destroy(ai.WorkingMemory.GetItem<GameObject>("search_waypoint").gameObject);
+            ai.WorkingMemory.RemoveItem("search_waypoint");
 
-		ai.WorkingMemory.SetItem<GameObject> ("search_waypoint",obj);
+        }
+        Vector3 UltPos = ai.WorkingMemory.GetItem<GameObject>("EnemyGoPersistent").transform.position;
+        GameObject obj = new GameObject();
+        WaypointRig WaypointRigTemp = obj.AddComponent<WaypointRig>();
+        //se Player fora do alcance
+        WaypointRigTemp.WaypointSet.SetType = 0;
+        WaypointRigTemp.WaypointSet.AddWaypoint(new Waypoint(UltPos, "UltimaPosicao"));
+        Vector3 Step = UltPos + (UltPos - ai.Kinematic.Position);
+        float rand = Random.Range(1f, 360f);
+        WaypointRigTemp.WaypointSet.AddWaypoint(new Waypoint(new Vector3(Step.x + Mathf.Cos(rand) * 10f, Step.y, Step.z + Mathf.Sin(rand) * 10f), "circulo 1"));
+        rand = Random.Range(1f, 360f);
+        WaypointRigTemp.WaypointSet.AddWaypoint(new Waypoint(new Vector3(Step.x + Mathf.Cos(rand) * 10f, Step.y, Step.z + Mathf.Sin(rand) * 10f), "quadrado 2"));
+        rand = Random.Range(1f, 360f);
+        WaypointRigTemp.WaypointSet.AddWaypoint(new Waypoint(new Vector3(Step.x + Mathf.Cos(rand) * 10f, Step.y, Step.z + Mathf.Sin(rand) * 10f), "quadrado 3"));
+        rand = Random.Range(1f, 360f);
+        WaypointRigTemp.WaypointSet.AddWaypoint(new Waypoint(new Vector3(Step.x + Mathf.Cos(rand) * 10f, Step.y, Step.z + Mathf.Sin(rand) * 10f), "quadrado 4"));
+        
+
+        ai.WorkingMemory.SetItem<GameObject>("search_waypoint", obj);
 
 
     }
